c0d6cf4263e4d0ff45006f445b3dd7e8a6d7c7c8
abhishekvshett <abhishekvshet04@gmail.com>
2026-02-19 15:14:41 +0530

    task1

---
+++ Day4/task1.js
@@ -0,0 +1,12 @@
+let marks = 41;
+
+if (marks <= 100 && marks >= 75) {
+    console.log("Grade A");
+} else if (marks >= 60 && marks <= 74) {
+    console.log("Grade B");
+} else if (marks >= 40 && marks <= 59) {
+    console.log("Grade C");
+} else {
+    console.log("Fail");
+}
+
